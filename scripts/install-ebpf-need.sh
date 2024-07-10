99e3ae341970bca18ba9a75920efc7d64dec0a6d
backman <backman.only@gmail.com>
2024-07-10 22:08:52 +0800

    update script

--- scripts/install-ebpf-need.sh
+++ scripts/install-ebpf-need.sh
@@ -8,11 +8,11 @@ set -eux
 
 
 sudo apt-get update
-sudo apt upgrade
 
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 sudo ./llvm.sh 17 all
+
 
 sudo apt-get install -y bpfcc-tools "linux-headers-$(uname -r)"
 sudo apt-get install -y zsh
@@ -22,36 +22,12 @@ sudo apt install -y  pkg-config
 sudo apt-get install -y zlib1g-dev
 
 
-#goenv
-git clone https://github.com/go-nv/goenv.git ~/.goenv
-
-
-
-
-
-echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.bash_profile
-echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(goenv init -)"' >> ~/.bash_profile
-echo 'export PATH="$GOROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bash_profile
-
-echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.zshrc
-echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(goenv init -)"' >> ~/.zshrc
-echo 'export PATH="$GOROOT/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.zshrc
-
-
-
-
-
-sudo apt --fix-broken install
 sudo apt install -y zip bison build-essential cmake flex git libedit-dev  zlib1g-dev libelf-dev libfl-dev python3-setuptools liblzma-dev arping netperf iperf 
+
+sudo git clone --recurse-submodules https://github.com/libbpf/libbpf-bootstrap
+sudo git submodule update --init --recursive 
+
 
 git clone --recurse-submodules https://github.com/libbpf/bpftool.git
 cd ./bpftool/src && sudo make install
 
-sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
-
-
-#manual install go
